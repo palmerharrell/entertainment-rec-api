dc4e9bb536913c74461db0dfdbb4ed12eff5b0c6
Palmer Harrell <palmerharrell@me.com>
2016-06-14 12:01:21 -0500

    Removing unused blank methods in MediaTypeController. Simplifying GET method in MediaTypeController.

--- MediaAPI/Controllers/MediaTypeController.cs
+++ MediaAPI/Controllers/MediaTypeController.cs
@@ -29,35 +29,15 @@ namespace MediaAPI.Controllers
         return BadRequest(ModelState);
       }
 
-      IQueryable<MediaType> mediaTypes = from mt in _context.MediaType
-                                        select mt;
+      IQueryable<object> mediaTypes = from mt in _context.MediaType
+                                         select new
+                                         {
+                                           Name = mt.Name,
+                                           ColorLight = mt.ColorLight,
+                                           ColorDark = mt.ColorDark
+                                         };
 
       return Ok(mediaTypes);
-    }
-
-    // GET api/values/5
-    [HttpGet("{id}")]
-    public string Get(int id)
-    {
-      return "value";
-    }
-
-    // POST api/values
-    [HttpPost]
-    public void Post([FromBody]string value)
-    {
-    }
-
-    // PUT api/values/5
-    [HttpPut("{id}")]
-    public void Put(int id, [FromBody]string value)
-    {
-    }
-
-    // DELETE api/values/5
-    [HttpDelete("{id}")]
-    public void Delete(int id)
-    {
     }
   }
 }
